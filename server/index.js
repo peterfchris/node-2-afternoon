af2040200f036b9820d2f99110d1d38474e6610d
Peter Forsgren Christensen <peter.f.chris@gmail.com>
2019-05-07 16:38:00 -0600

    steps 1 through 3 complete

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const port = 3001
+applisten(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
